a0045d8c354f92e70be053cdb5bb5ada4c92651e
jjescobar <jjescobar@uninorte.edu.co>
2022-02-01 22:19:25 -0500

    defining responsive layout

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_flutter/utils/colors.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Instagram Clone',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: mobileBackgroundColor,
+      ),
+      home: const Scaffold(body: Text('Lets build Instagram')),
+    );
+  }
+}
