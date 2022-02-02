670a55e6b3f9ee28ec200cf1527a136537bc04fd
jjescobar <jjescobar@uninorte.edu.co>
2022-02-01 22:47:56 -0500

    integrate mobiel and web screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_flutter/responsive/mobile_screen_layout.dart';
+import 'package:instagram_flutter/responsive/responsive_layout_screen.dart';
+import 'package:instagram_flutter/responsive/web_screen_layout.dart';
 import 'package:instagram_flutter/utils/colors.dart';
 
 void main() {
@@ -16,7 +19,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: mobileBackgroundColor,
       ),
-      home: const Scaffold(body: Text('Lets build Instagram')),
+      home: const ResponsiveLayout(
+        webScreenLayout: WebScreenLayout(),
+        mobileScreenLayout: MobileScreenLayout(),
+      ),
     );
   }
 }
--- lib/responsive/responsive_layout_screen.dart
+++ lib/responsive/responsive_layout_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:instagram_flutter/utils/dimensions.dart';
 
 class ResponsiveLayout extends StatelessWidget {
